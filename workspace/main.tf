d6c8655cc508dbb13ce86940f2e59aaad60f90fa
Andrew Way <andrew.way@armory.io>
2020-07-20 22:41:51 -0700

    Update main.tf

--- workspace/main.tf
+++ workspace/main.tf
@@ -12,7 +12,7 @@ variable "environment_name" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "spinnaker-test-${var.environment_name}"
+  bucket = "tfe-${var.environment_name}"
   acl    = "public-read"
 
   tags = {
