68627d4a90c8e85069b9f6131badfec71f98b463
Andrew Way <andrew.way@armory.io>
2020-07-20 19:02:02 -0700

    make bucket private

--- workspace/main.tf
+++ workspace/main.tf
@@ -13,7 +13,7 @@ variable "environment_name" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "terraformer-${var.environment_name}"
-  acl    = "public-read"
+  acl    = "private"
 
   tags = {
     Name = "Bucket for ${var.environment_name}"
