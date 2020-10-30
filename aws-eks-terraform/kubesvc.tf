dc8b6aec8304e34e6ce8e38f142e860b76d0cf7a
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-10-29 19:22:32 -0700

    Create kubesvc.tf

---
+++ aws-eks-terraform/kubesvc.tf
@@ -0,0 +1,27 @@
+variable "account" {
+  type = string
+}
+
+variable "grpcurl" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "environmentId" {
+  type = string
+}
+
+resource "null_resource" "kubectl" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl kustomize \
+        | sed "s/grpc_url_replace/${var.grpcurl}/g" \
+        | sed "s/saas_spinnaker_account_replace/${var.account}-${var.env}-saas/g" \
+        | kubectl apply --kubeconfig ../../accounts/saas-customers/${var.environmentId}/kubeconfig -f -
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
