d9f1c3851be304206684de84de0a642aeddab93d
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-10-29 20:24:02 -0700

    Update vpc.tf

--- aws-eks-terraform/vpc.tf
+++ aws-eks-terraform/vpc.tf
@@ -11,7 +11,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "training-eks-${random_string.suffix.result}"
+  cluster_name = "armory-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -23,7 +23,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "training-vpc"
+  name                 = "armory-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
