bbd3b870a174fa4707bf72b88c811b2b58a898a6
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-11-10 21:51:13 -0800

    Update armory-agent.tf

--- aws-eks-terraform/agent-deploy-tf/armory-agent.tf
+++ aws-eks-terraform/agent-deploy-tf/armory-agent.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
 
