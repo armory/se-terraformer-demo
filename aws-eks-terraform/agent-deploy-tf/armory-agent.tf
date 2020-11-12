c27273aeab7dbe92e7e6897785fcaaa99976af28
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-11-11 19:50:09 -0800

    Update armory-agent.tf

--- aws-eks-terraform/agent-deploy-tf/armory-agent.tf
+++ aws-eks-terraform/agent-deploy-tf/armory-agent.tf
@@ -1,84 +1,13 @@
-resource "kubernetes_deployment" "spin_kubesvc" {
-  metadata {
-    name = "spin-kubesvc"
-
-    labels = {
-      app = "spin"
-      app.kubernetes.io/name = "kubesvc"
-      app.kubernetes.io/part-of = "spinnaker"
-      cluster = "spin-kubesvc"
-    }
+resource "null_resource" "install-agent" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kustomize build ../agent-deployment \
+        | sed "s/account_name_replace/account_name_replace/g" \
+        | kubectl apply --kubeconfig ../kubecfgs/kubecfg-demo.yaml -f -
+    EOT
+    interpreter = ["/bin/bash", "-c"]
   }
-
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "spin"
-        cluster = "spin-kubesvc"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "spin"
-          app.kubernetes.io/name = "kubesvc"
-          app.kubernetes.io/part-of = "spinnaker"
-          cluster = "spin-kubesvc"
-        }
-        annotations = {
-          prometheus.io/path = "/metrics"
-          prometheus.io/port = "8008"
-          prometheus.io/scrape = "true"
-        }
-      }
-      spec {
-        volume {
-          name = "volume-kubesvc-config"
-          config_map {
-            name = "kubesvc-config"
-          }
-        }
-        container {
-          name  = "kubesvc"
-          image = "armory/kubesvc"
-
-          port {
-            name           = "health"
-            container_port = 8082
-            protocol       = "TCP"
-          }
-
-          port {
-            name           = "metrics"
-            container_port = 8008
-            protocol       = "TCP"
-          }
-
-          volume_mount {
-            name       = "volume-kubesvc-config"
-            mount_path = "/opt/spinnaker/config"
-          }
-
-          readiness_probe {
-            http_get {
-              path = "/health"
-              port = "health"
-            }
-
-            timeout_seconds   = 1
-            period_seconds    = 10
-            success_threshold = 1
-            failure_threshold = 3
-          }
-
-          termination_message_path = "/dev/termination-log"
-          image_pull_policy        = "IfNotPresent"
-        }
-
-        restart_policy       = "Always"
-        service_account_name = "spin-sa"
-      }
-    }
-  }
+  depends_on = [
+    null_resource.get-credentials,
+  ]
 }
