e3fe33df2c66fcff20fc44505400c3cca6c0c28d
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-11-12 21:02:41 -0800

    Update armory-agent.tf

--- aws-eks-terraform/agent-deploy-tf/armory-agent.tf
+++ aws-eks-terraform/agent-deploy-tf/armory-agent.tf
@@ -2,7 +2,7 @@ resource "null_resource" "install-agent" {
   count = 50
   provisioner "local-exec" {
     command = <<-EOT
-      kubectl apply --kubeconfig ../kubecfgs/serviceaccount.yaml
+      kubectl apply -f ./agent-deployment/serviceaccount.yaml
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
