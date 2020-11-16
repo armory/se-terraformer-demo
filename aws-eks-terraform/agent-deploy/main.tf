3356c64657268fd2762704d64b48d5d8a2daee04
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-11-16 10:10:52 -0800

    Update main.tf

--- aws-eks-terraform/agent-deploy/main.tf
+++ aws-eks-terraform/agent-deploy/main.tf
@@ -3,8 +3,7 @@ resource "null_resource" "kubectl" {
     command = <<-EOT
       kubectl kustomize \
         | sed "s/grpc_url_replace/${var.grpcurl}/g" \
-        | sed "s/saas_spinnaker_account_replace/${var.account}-${var.env}-saas/g" \
-        | kubectl apply --kubeconfig ../../accounts/saas-customers/${var.environmentId}/kubeconfig -f -
+        | sed "s/saas_spinnaker_account_replace/${var.account}-${var.env}-saas/g" -f -
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
