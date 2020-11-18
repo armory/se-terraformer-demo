0334aeab25a24c369b6092271ce81d0fb3b71b37
chadtripod <54915503+chadtripod@users.noreply.github.com>
2020-11-18 09:24:54 -0800

    Update main.tf

--- aws-eks-terraform/agent-deploy/main.tf
+++ aws-eks-terraform/agent-deploy/main.tf
@@ -1,10 +1,3 @@
-provider "kubernetes" {
-  load_config_file       = "false"
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-}
-
 variable "grpcurl" {
   default = "host-clouddriver.plugin.fqdn.com"
 }
@@ -17,12 +10,17 @@ variable "kubeconfig" {
   default = "kubeconfig_default"
 }
 
+resource "local_file" "kubeconfig" {
+    content     = var.kubeconfig
+    filename = "${path.module}/kubeconfig"
+}
+
 resource "null_resource" "kubectl" {
   provisioner "local-exec" {
     command = <<-EOT
       kubectl kustomize \
         | sed "s/grpc_url_replace/${var.grpcurl}/g" \
-        | sed "s/saas_spinnaker_account_replace/${var.account}/g" | kubectl --kubeconfig ${var.kubeconfig} apply -f -
+        | sed "s/saas_spinnaker_account_replace/${var.account}/g" | kubectl --kubeconfig ${local_file.kubeconfig.filename} apply -f -
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
