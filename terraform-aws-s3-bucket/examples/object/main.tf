92dc62b688631b14b1be358ed2ce6bdf4bf5c548
Nate Reid <45366367+natereid72@users.noreply.github.com>
2021-09-01 16:34:40 -0400

    Update main.tf

--- terraform-aws-s3-bucket/examples/object/main.tf
+++ terraform-aws-s3-bucket/examples/object/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "armory-se-terraformer-demo"
     key     = "demo-api-dev.tfstate"
-    region  = "us-west-2"
+    region  = "us-east-1"
     encrypt = false
    profile = "terraform"
   }
