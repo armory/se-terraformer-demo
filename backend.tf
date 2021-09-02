3fe3aea1eeb1c883f03f6f6b46363d930ab0ed80
Nate Reid <45366367+natereid72@users.noreply.github.com>
2021-09-02 11:39:40 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 bucket  = "armory-se-terraformer-demo"
-key     = ${execution['application']-execution['name']}
+key     = "${execution['application']-execution['name']}"
 region  = "us-east-1"
 encrypt = false
 profile = "terraform"
